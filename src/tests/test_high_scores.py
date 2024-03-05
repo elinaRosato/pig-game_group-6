0028ab5cf347a17a85335731eea5a4b99365dc8b
Simon Persson <atlaspersson@outlook.com>
2024-03-06 00:37:57 +0100

    added tearDown method and test_initialization method

--- src/tests/test_high_scores.py
+++ src/tests/test_high_scores.py
@@ -4,7 +4,20 @@ import highscore
 
 
 class test_high_score(unittest.TestCase):
-
+    #This method sets up the filename and highscore as parameters
+    # to be used by the other methods
     def setUp(self):
         self.filename = "test_highscores.json"
         self.highscore = highscore.HighScore(self.filename)
+        
+    #This method cleans up the temporary file resources after
+    #launching the meethods so they don´t interfere with eachother
+    def tearDown(self):
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
+
+    #This method tests the initialization of the high score object
+    def test_initialization(self):
+        """Test initialization of HighScore."""
+        self.assertTrue(os.path.exists(self.filename))
+        self.assertIsInstance(self.highscore.highscores, dict)
