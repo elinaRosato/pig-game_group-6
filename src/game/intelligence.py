d9ab170911603cec3d7219dee083c23632686eb7
Elina <rosatoelina@gmail.com>
2024-03-06 20:18:54 +0100

    created __init__ method for intelligence class

--- src/game/intelligence.py
+++ src/game/intelligence.py
@@ -2,3 +2,11 @@ import random
 
 class Intelligence:
     levels = {"easy", "medium", "hard"}
+
+    def __init__(self, difficulty="medium"):
+        if not isinstance(difficulty, str):
+            raise TypeError("Difficulty level must be a string.")
+        if difficulty not in self.levels:
+            raise ValueError("Invalid difficulty level. Valid difficulty levels are 'easy', 'medium', or 'hard'.")
+        else:
+            self.difficulty = difficulty
