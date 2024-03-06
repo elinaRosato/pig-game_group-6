c21be92f7b811aeb47e8757abf1c0f3cf2738cba
Dario Ostojic <darioostojic2@gmail.com>
2024-03-06 21:17:02 +0100

    Added following tests to TestDiceHand class: test_roll_dice_invalid_input_negative_integer & test_roll_dice_positive_integer

--- src/tests/test_dice_hand.py
+++ src/tests/test_dice_hand.py
@@ -9,3 +9,16 @@ class TestDiceHand(unittest.TestCase):
     def test_roll_dice_invalid_input_zero(self):
         with self.assertRaises(ValueError):
             self.dice_hand.roll_dice(0)
+            
+    def test_roll_dice_invalid_input_negative_integer(self):
+        with self.assertRaises(ValueError):
+            self.dice_hand.roll_dice(-1)
+            
+    def test_roll_dice_positive_integer(self):
+        num_rolls = 5
+        rolls = self.dice_hand.roll_dice(num_rolls)
+        self.assertEqual(len(rolls), num_rolls)
+        for roll in rolls:
+            self.assertIsInstance(roll, int)
+            self.assertGreaterEqual(roll, 1)
+            self.assertLessEqual(roll, 6)
