4f7c8b7130c3133d4d757c07bd8ba1d5028b1741
Elina <rosatoelina@gmail.com>
2024-03-06 17:42:25 +0100

    created invalid player initialization test methods

--- src/tests/test_player.py
+++ src/tests/test_player.py
@@ -26,3 +26,25 @@ class TestPlayer(unittest.TestCase):
         player = Player("Alice", False)
         self.assertEqual(player.name, "Alice")
         self.assertFalse(player.is_computer)
+
+    #Invalid Player Initialization
+    def test_invalid_player_initialization_with_integer(self):
+        """
+        Test creating a player with an invalid name, in this case an integer, and expect a TypeError.
+        """
+        with self.assertRaises((TypeError)):
+            invalid_player = Player(123)
+    
+    def test_invalid_player_initialization_with_empty_string(self):
+        """
+        Test creating a player with an invalid name, in this case an empty string, and expect a ValueError.
+        """
+        with self.assertRaises((ValueError)):
+            invalid_player = Player("")
+
+    def test_invalid_player_initialization_with_invalid_is_computer(self):
+        """
+        Test creating a player with an invalid is_computer value instead of a boolean and expect a TypeError.
+        """
+        with self.assertRaises((TypeError)):
+            invalid_player = Player("Alice", 123)
