44db7ead3e58c854191022a26098ef225734c643
Elina <rosatoelina@gmail.com>
2024-03-06 18:44:28 +0100

    created invalid intelligence intit test methods

--- src/tests/test_intelligence.py
+++ src/tests/test_intelligence.py
@@ -30,6 +30,35 @@ class TestIntelligence(unittest.TestCase):
         """
         intelligence_instance = Intelligence("hard")
         self.assertEqual(intelligence_instance.difficulty, "hard")
+    
+    # Invalid Intelligence Initialization
+    def test_invalid_intelligence_initialization_type(self):
+        """
+        Test creating an Intelligence instance with an invalid difficulty type and expect a TypeError.
+        """
+        # Test case 1: Invalid level type (integer)
+        with self.assertRaises(TypeError):
+            intelligence_instance = Intelligence(123)
+
+        # Test case 2: Invalid level type (float)
+        with self.assertRaises(TypeError):
+            intelligence_instance = Intelligence(1.23)
+        
+        # Test case 3: Invalid level type (boolean)
+        with self.assertRaises(TypeError):
+            intelligence_instance = Intelligence(False)
+
+    def test_invalid_intelligence_initialization_value(self):
+        """
+        Test creating an Intelligence instance with an invalid difficulty value and expect a ValueError.
+        """
+        # Test case 1: Invalid level type (empty string)
+        with self.assertRaises(ValueError):
+            intelligence_instance = Intelligence("")
+
+        # Test case 2: Invalid level type (invalid string)
+        with self.assertRaises(ValueError):
+            intelligence_instance = Intelligence("difficult")
 
 if __name__ == '__main__':
     unittest.main()
