e56cd8f03f01a6837f1b971afdf49ec7b2e941a9
Elina <rosatoelina@gmail.com>
2024-03-07 23:13:07 +0100

    added game class constructor

--- src/game/game.py
+++ src/game/game.py
@@ -5,6 +5,28 @@ import intelligence
 import histogram
 
 class Game:
+    """
+    Represents the game engine.
+
+    Attributes:
+    dice_hand (DiceHand): An instance of the DiceHand class for rolling dice.
+    - high_score (HighScore): An instance of the HighScore class for tracking high scores.
+    - player1 (Player): An instance of the Player class representing the first player.
+    - player2 (Player): An instance of the Player class representing the second player.
+    - intelligence (Intelligence): An instance of the Intelligence class for computer opponent decisions.
+    - histogram (Histogram): An instance of the Histogram class for tracking dice roll frequencies.
+    """
+
+    def __init__(self):
+        """
+        Initializes a new game with default settings.
+        """
+        self.dice_hand = dicehand.DiceHand()
+        self.high_score = highscores.HighScore()
+        self.player1 = player.Player("Player1")
+        self.player2 = player.Player("Player2")
+        self.intelligence = intelligence.Intelligence()
+        self.histogram = histogram.Histogram()
 
 
 if __name__ == "__main__":
