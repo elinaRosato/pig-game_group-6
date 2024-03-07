667154c866a9ec7b9213c80844996ea5602edaf5
Elina <rosatoelina@gmail.com>
2024-03-07 19:49:22 +0100

    created setUp method

--- src/tests/test_histogram.py
+++ src/tests/test_histogram.py
@@ -3,6 +3,8 @@ from unittest.mock import patch
 from game import Histogram
 
 class TestHistogram(unittest.TestCase):
+    def setUp(self):
+        self.histogram = Histogram()
 
 if __name__ == '__main__':
     unittest.main()
