eb3e528fd2ac7a24901f1d2700db87d6962189c4
Elina <rosatoelina@gmail.com>
2024-03-08 00:10:27 +0100

    expanded game test methods

--- src/tests/test_game.py
+++ src/tests/test_game.py
@@ -33,22 +33,23 @@ class TestGame(unittest.TestCase):
     def test_get_player_name(self):
         """
         Test the get_player_name method.
-
-        Mocks the input function to provide the name 'John'.
-        Checks if the player1's name is correctly set to 'John'.
         """
         with patch('builtins.input', side_effect=['John']):
             self.game.get_player_name()
         self.assertEqual(self.game.player1.name, 'John')
 
+    def test_get_player_name_empty_input(self):
+        """
+        Test handling an empty string input in get_player_name method.
+        """
+        with patch('builtins.input', side_effect=['', 'Victoria']):
+            self.game.get_player_name()
+        self.assertEqual(self.game.player1.name, 'Victoria')
+
     # Change player name
     def test_change_player_name(self):
         """
         Test the change_player_name method.
-
-        Sets the player1's name to 'Sarah', updates highscores with the old name,
-        mocks the input to change the name to 'Peter', and checks if the player1's
-        name is correctly updated to 'Peter'.
         """
         self.game.player1.change_name("Sarah")
         
@@ -58,14 +59,23 @@ class TestGame(unittest.TestCase):
             self.game.change_player_name()
         
         self.assertEqual(self.game.player1.name, 'Peter')
+    
+    def test_change_player_name_empty_input(self):
+        """
+        Test handling an empty string input in change_player_name method.
+        """
+        self.game.player1.change_name("Dora")
+        self.game.high_score.update_highscores("Dora", 0)
         
+        with patch('builtins.input', side_effect=['yes', '']):
+            self.game.change_player_name()
+        
+        self.assertEqual(self.game.player1.name, 'Dora')
+    
     # Set computer opponent
     def test_set_computer_opponent_with_valid_difficulty(self):
         """
         Test the set_computer_opponent method with a valid difficulty.
-
-        Mocks the input to choose 'easy' difficulty for the computer opponent.
-        Checks if player2 is marked as a computer and if the intelligence difficulty is set correctly.
         """
         with patch('builtins.input', side_effect=['easy']):
             self.game.set_computer_opponent()
@@ -76,15 +86,15 @@ class TestGame(unittest.TestCase):
     def test_set_human_opponent(self):
         """
         Test the set_human_opponent method.
-
-        Mocks the input to provide the opponent's name as 'Benjamin'.
-        Checks if player2's name is correctly set to 'Benjamin'.
         """
         with patch('builtins.input', return_value='Benjamin'):
             self.game.set_human_opponent()
         self.assertEqual(self.game.player2.name, 'Benjamin')
         
     def test_play_round_2_human_players(self):
+        """
+        Test the play_round method with two human players.
+        """
         # Prepare predefined inputs for player decision
         player1_inputs = ['roll', '1', 'hold']
         player2_inputs = ['roll', '1', 'cheat']
