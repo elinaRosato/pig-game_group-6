67387e9ea1c24be6133cbe84b3b4bb3d62391ae3
Elina <rosatoelina@gmail.com>
2024-03-07 19:04:43 +0100

    added game init test method

--- src/tests/test_game.py
+++ src/tests/test_game.py
@@ -4,5 +4,18 @@ from game import Game
 
 class TestGame(unittest.TestCase):
 
+    # Game Initialization
+    def test_game_initialization(self):
+        """
+        Test the initialization of the Game class.
+        """
+        game = Game()
+        self.assertIsNotNone(game.dice_hand)
+        self.assertIsNotNone(game.high_score)
+        self.assertIsNotNone(game.player1)
+        self.assertIsNotNone(game.player2)
+        self.assertIsNotNone(game.intelligence)
+        self.assertIsNotNone(game.histogram)
+
 if __name__ == '__main__':
     unittest.main()
