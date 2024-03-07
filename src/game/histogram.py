48dabcbf47aae9a6daa707b1bbebfaaba0a1910b
Elina <rosatoelina@gmail.com>
2024-03-07 20:34:00 +0100

    created add roll method

--- src/game/histogram.py
+++ src/game/histogram.py
@@ -15,3 +15,20 @@ class Histogram:
         self.counts = {}
         for i in range(1, 7):
             self.counts[i] = 0
+    
+    def add_roll(self, roll):
+        """
+        Adds a dice roll to the histogram.
+
+        Args:
+        - roll (int): The value of the dice roll.
+
+        Raises:
+        - TypeError: If the roll is not an integer.
+        - ValueError: If the roll is not within the range [1, 6].
+        """
+        if not isinstance(roll, int) or roll is True or roll is False:
+            raise TypeError("Roll must be an integer.")
+        if roll < 1 or roll > 6:
+            raise ValueError("Roll must be an integer within the range [1, 6].")
+        self.counts[roll] += 1
