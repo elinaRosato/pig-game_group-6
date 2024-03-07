f9cd1e91aa4f9a6b9683778a50955693567b6461
Simon Persson <atlaspersson@outlook.com>
2024-03-07 12:45:10 +0100

    Pushed test_high_scores.py

--- src/game/high_scores.py
+++ src/game/high_scores.py
@@ -1,5 +1,12 @@
 import json
 import os
 
-
+#This class 
 class HighScore:
+    #This method
+    def __init__(self, filename="highscores.json"):
+        if not isinstance(filename, str):
+            raise TypeError("Filename must be a string.")
+        self.filename = filename
+        self.create_file_if_not_exists()
+        self.highscores = self.load_highscores()
