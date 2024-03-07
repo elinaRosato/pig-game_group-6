1083cfdc113e4e50760e15ae168be36dc1b96f9d
Dario Ostojic <darioostojic2@gmail.com>
2024-03-07 10:10:40 +0100

    fixed indentation in _init_.py

--- src/game/__init__.py
+++ src/game/__init__.py
@@ -1,2 +1,2 @@
-#We need this file in order to generate the uml documentation due to an issue in the pyreverse library:
+# We need this file in order to generate the uml documentation due to an issue in the pyreverse library:
 # https://github.com/pylint-dev/pylint/issues/2763
