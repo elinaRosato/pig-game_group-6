dcc8035ed09c9e3b9d06d6169e2f38e6601b5774
Elina <rosatoelina@gmail.com>
2024-03-07 19:53:26 +0100

    added histogram init tests

--- src/tests/test_histogram.py
+++ src/tests/test_histogram.py
@@ -3,8 +3,17 @@ from unittest.mock import patch
 from game import Histogram
 
 class TestHistogram(unittest.TestCase):
+
+    # Set up
     def setUp(self):
         self.histogram = Histogram()
+    
+    def test_initialization(self):
+        """
+        Test the initialization of the Histogram class.
+        """
+        expected_counts = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
+        self.assertEqual(self.histogram.counts, expected_counts)
 
 if __name__ == '__main__':
     unittest.main()
