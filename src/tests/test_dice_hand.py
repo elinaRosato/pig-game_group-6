d76dac5fdf592f966696e9e4e4891a40f36291f4
Dario Ostojic <darioostojic2@gmail.com>
2024-03-07 12:20:15 +0100

    Updated TestDiceHand class tests with test descriptions

--- src/tests/test_dice_hand.py
+++ src/tests/test_dice_hand.py
@@ -7,14 +7,17 @@ class TestDiceHand(unittest.TestCase):
         self.dice_hand = dice_hand.DiceHand()
         
     def test_roll_dice_invalid_input_zero(self):
+        """Test if rolling the dice with zero as an integer input raises ValueError."""
         with self.assertRaises(ValueError):
             self.dice_hand.roll_dice(0)
             
     def test_roll_dice_invalid_input_negative_integer(self):
+        """Test if rolling the dice with a negative integer as an input raises ValueError."""
         with self.assertRaises(ValueError):
             self.dice_hand.roll_dice(-1)
             
     def test_roll_dice_positive_integer(self):
+        """Test if rolling the dice with a positive integer as an input returns a list of results."""
         num_rolls = 5
         rolls = self.dice_hand.roll_dice(num_rolls)
         self.assertEqual(len(rolls), num_rolls)
@@ -24,30 +27,39 @@ class TestDiceHand(unittest.TestCase):
             self.assertLessEqual(roll, 6)
             
     def test_roll_dice_invalid_input_negative_float(self):
+        """Test if rolling the dice with negative float as an input raises ValueError.
+           First it's checked if the number is negative and later its type is checked."""
         with self.assertRaises(ValueError):
             self.dice_hand.roll_dice(-1.5)
 
     def test_roll_dice_invalid_input_positive_float(self):
+        """Test if rolling the dice with positive float as an input raises TypeError."""
         with self.assertRaises(TypeError):
             self.dice_hand.roll_dice(1.5)
             
     def test_roll_dice_invalid_input_letter(self):
+        """Test if rolling the dice with letter as an input raises TypeError."""
         with self.assertRaises(TypeError):
             self.dice_hand.roll_dice('d')
             
     def test_roll_dice_invalid_input_None(self):
+        """Test if rolling the dice with None as an input raises TypeError."""
         with self.assertRaises(TypeError):
             self.dice_hand.roll_dice(None)
             
     def test_roll_dice_invalid_input_true_boolean(self):
+        """Test if rolling the dice with boolean True as an input raises TypeError."""
         with self.assertRaises(TypeError):
             self.dice_hand.roll_dice(True)
 
     def test_roll_dice_invalid_input_false_boolean(self):
+        """Test if rolling the dice with boolean False as an input raises TypeError."""
         with self.assertRaises(TypeError):
             self.dice_hand.roll_dice(False)
             
     def test_roll_dice_count_matches_result_count(self):
+        """Test that when rolling the dice a specified number of times (num_rolls), the length
+           of the resulting list of integer values (rolls) matches with the number of rolls."""   
         num_rolls = 100
         rolls = self.dice_hand.roll_dice(num_rolls)
         self.assertEqual(len(rolls), num_rolls)
