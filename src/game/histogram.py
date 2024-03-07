b960dea0d9b484ce2f1f4f9aad127387ccae1fd7
Elina <rosatoelina@gmail.com>
2024-03-07 20:31:24 +0100

    created histogram constructor

--- src/game/histogram.py
+++ src/game/histogram.py
@@ -5,3 +5,13 @@ class Histogram:
     Attributes:
     - counts (dict): A dictionary to store the count of each dice roll.
     """
+
+    def __init__(self):
+        """
+        Initializes a new instance of the Histogram class.
+
+        The counts dictionary is initialized with counts set to zero for each possible dice roll (1 to 6).
+        """
+        self.counts = {}
+        for i in range(1, 7):
+            self.counts[i] = 0
