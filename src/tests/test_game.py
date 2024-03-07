883bcf2ab2f2a4ba67540793ca00b5539e94fe53
Dario Ostojic <darioostojic2@gmail.com>
2024-03-07 22:25:20 +0100

    Added in TestGame tests for giving the name and updating it.

--- src/tests/test_game.py
+++ src/tests/test_game.py
@@ -1,8 +1,18 @@
 import unittest
 from unittest.mock import patch
 from game import Game
+import os
 
 class TestGame(unittest.TestCase):
+        
+    def setUp(self):
+        self.game = Game()
+        
+    def tearDown(self):
+        #We need this to reset the file of highscores and avoid conflicts with existing names from previous test executions.
+        filename = "highscores.json"
+        if os.path.exists(filename):
+            os.remove(filename)    
 
     # Game Initialization
     def test_game_initialization(self):
@@ -16,6 +26,21 @@ class TestGame(unittest.TestCase):
         self.assertIsNotNone(game.player2)
         self.assertIsNotNone(game.intelligence)
         self.assertIsNotNone(game.histogram)
+        
+    def test_get_player_name(self):
+        with patch('builtins.input', side_effect=['John']):
+            self.game.get_player_name()
+        self.assertEqual(self.game.player1.name, 'John')
+
+    def test_change_player_name(self):
+        self.game.player1.change_name("Sarah")
+        
+        self.game.high_score.update_highscores("Sarah", 0) 
+        
+        with patch('builtins.input', side_effect=['yes', 'Peter']):
+            self.game.change_player_name()
+        
+        self.assertEqual(self.game.player1.name, 'Peter')
 
 if __name__ == '__main__':
     unittest.main()
